427edc3de477d1db77d690d936a1f55b6f0df4de
Xavier Pouyat <xpouyat@microsoft.com>
2022-02-04 16:47:56 +0100

    Update for cert. Update for Terraform, scripts and the fact default branch is main and not master

--- infrastructure/terraform/bashscriptgenerator/templates/logic_app_sp.sh
+++ infrastructure/terraform/bashscriptgenerator/templates/logic_app_sp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-az role assignment create --role "Website Contributor" --assignee-object-id ${logicAppSCHServicePrincipalId} --scope ${functionAppId}
+az role assignment create --role "Website Contributor" --assignee-object-id ${logicAppSCHServicePrincipalId} --scope ${functionAppId} --assignee-principal-type ServicePrincipal
 
 for acct in ${storageAccountIds}
 {
     echo "Granting Logic App access to $acct"
-    az role assignment create --role "Storage Account Key Operator Service Role" --assignee-object-id ${logicAppKRServicePrincipalId} --scope $acct
+    az role assignment create --role "Storage Account Key Operator Service Role" --assignee-object-id ${logicAppKRServicePrincipalId} --scope $acct --assignee-principal-type ServicePrincipal
 }
 
 for id in ${storageRgIds}
 {
     echo "Granting Logic App access to $id"
-    az role assignment create --role "Reader and Data Access" --assignee-object-id ${logicAppKRServicePrincipalId} --scope $id
+    az role assignment create --role "Reader and Data Access" --assignee-object-id ${logicAppKRServicePrincipalId} --scope $id --assignee-principal-type ServicePrincipal
 }
